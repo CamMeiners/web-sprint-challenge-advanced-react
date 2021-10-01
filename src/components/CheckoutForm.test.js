907b59b9aff5e1a4d29f9a649ab4ef65e3ee3626
CamMeiners <cammeiners@gmail.com>
2021-10-01 02:27:15 -0600

    finished everything but hooks! Will do it in the morning

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,31 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
+    render(<CheckoutForm/>)
+});
 
-test("shows success message on submit with form details", () => {});
+test("shows success message on submit with form details", () => {
+    render(<CheckoutForm/>)
+    const firstName = screen.getByLabelText(/First Name/);
+    userEvent.type(firstName, 'Cameron');
+    const lastName = screen.getByLabelText(/Last Name/);
+    userEvent.type(lastName, 'Meiners');
+    const addy = screen.getByLabelText(/Address/);
+    userEvent.type(addy, '1414 Johnson Ave');
+    const city = screen.getByLabelText(/City/);
+    userEvent.type(city, 'Jonestown');
+    const state = screen.getByLabelText(/State/);
+    userEvent.type(state, 'Kentucky');
+    const zippy = screen.getByLabelText(/Zip/);
+    userEvent.type(zippy, '91981');
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    const success = screen.queryByText(/You have ordered some plants!/)
+    expect(success).toBeInTheDocument;
+});
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,19 @@
 // write your custom hook here to control your checkout form
+import React from 'react';
+const useLocalStorage = (key, ininitalValue) => {
+    const [storedValue, setStoredValue] = useState(()=> {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : ininitalValue;
+    });
+    const setValue = (value) => {
+        window.localStorage.setItem(key, value);
+        setStoredValue(value);
+    }
+    return [storedValue, setValue]
+}
+
+
+const useForm = () => {
+const [value, setValue] = useLocalStorage('formValues', false);
+return [value, setValue];
+}
